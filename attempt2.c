208a7c895962e90f377a7f82c32c4779f24d2da8
Spenser Laier <spenserlaier@gmail.com>
2024-12-20 12:23:55 -0500

    add decoding

--- attempt2.c
+++ attempt2.c
@@ -12,7 +12,12 @@ int max(int n1, int n2) {
     return n1 > n2? n1 : n2;
 }
 
-char * lz77(char* input_string, int search_buff_size) {
+typedef struct {
+    char * string;
+    int length;
+} lz77_output;
+
+lz77_output * encode_lz77(char* input_string, int search_buff_size) {
     unsigned long input_length = strlen(input_string);
     if (input_length < 2*search_buff_size) {
         return NULL;
@@ -33,7 +38,6 @@ char * lz77(char* input_string, int search_buff_size) {
         output_string[output_string_idx++] = 0;
         output_string[output_string_idx++] = input_string[idx];
     }
-
     while (look_ahead_buff_right < input_length) {
         //iterate through the  search buffer looking for the longest match starting
         //at current character in lookahead buffer
@@ -65,10 +69,38 @@ char * lz77(char* input_string, int search_buff_size) {
     }
     //TODO: setting a char to 0 is equivalent to setting it to null terminator \0, which
     //creates issues when searching for the end of a string
-    return output_string;
+    lz77_output * output = malloc(sizeof(lz77_output));
+    output->length = output_string_idx;
+    output->string = output_string;
+    return output;
+}
+char * decode_lz77(lz77_output * encoded_input) {
+    //scan through the match length values of the output and add them together
+    //to determine the decoded output. in cases where match length is 0, add 1
+    int decoded_size = 0;
+    int idx = 1; // position of first match_size value
+    while (idx < encoded_input->length) {
+        int match_size = encoded_input->string[idx] == 0? 1 : encoded_input->string[idx];
+        decoded_size += match_size;
+        idx += 3;
+    }
+    char * decoded = malloc(sizeof(char)*decoded_size);
+    int decoded_idx = 0;
+    for (int i = 0; i< encoded_input->length; i += 3) {
+        char backwards_offset = encoded_input->string[i];
+        char match_length = encoded_input->string[i+1];
+        char character = encoded_input->string[i+2];
+        if (match_length != 0) {
+            for (int j = i - backwards_offset; j < i + match_length; j ++ ) {
+                decoded[decoded_idx++] = decoded[j];
+            }
+        } else {
+            decoded[decoded_idx++] = character;
+        }
+    }
+    return decoded;
 }
 
 int main() {
-
     return 0;
 }
